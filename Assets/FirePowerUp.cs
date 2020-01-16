095b4810da16e1d4b5157cb3af3c982f17332a55
Julia Bouvier <juliabouv@gmail.com>
2020-01-15 18:00:25 -0800

    Adding Powerups

---
+++ Assets/FirePowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePowerUp : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Instantiate(pickupEffect, transform.position, Quaternion.identity);
+
+
+
+        Destroy(gameObject);
+    }
+}
