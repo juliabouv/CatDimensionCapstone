1f5122c93ae086e93b08b3270c92498adba3c828
Julia Bouvier <juliabouv@gmail.com>
2020-01-16 01:15:32 -0800

    Added additional powerups

---
+++ Assets/Scripts/GrowPowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowPowerUp : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+    public float multiplier = 1.4f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Pickup(collision);
+        }
+    }
+
+    void Pickup(Collider2D player)
+    {
+        Instantiate(pickupEffect, transform.position, Quaternion.identity);
+
+        player.transform.localScale *= multiplier;
+
+        Destroy(gameObject);
+    }
+}
